db0f73ed132d4b393cdc532ede213a00b5561e72
Luis Camargo <luismiguelcamar@gmail.com>
2022-10-22 00:17:38 +0000

    My first commit

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,26 +8,44 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    phone = Column(String(250), nullable=False)
+    password = Column(String, nullable=False)
+    created_at = Column(DateTime)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Type_post(Base):
+    __tablename__ = 'type_post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250))
 
-    def to_dict(self):
-        return {}
+class Like(Base):
+    __tablename__ = 'like'
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer, ForeignKey('user.id'))
+    id_post = Column(Integer, ForeignKey('post.id'))
+    date = Column(DateTime)
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    text = Column(String(250), primary_key=True)
+    id_user = Column(Integer, ForeignKey('user.id'))
+    date = Column(DateTime)
+    id_post = Column(Integer, ForeignKey('post.id'))
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer, ForeignKey('user.id'))
+    date = Column(DateTime)
+    id_type_post = Column(Integer, ForeignKey('type_post.id'))
+    text = Column(String)
+    id_post_shared = Column(Integer, ForeignKey('post.id'))
+
+    #def to_dict(self):
+        #return {}
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
